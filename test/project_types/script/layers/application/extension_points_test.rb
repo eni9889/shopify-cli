023e8c421862816a4288884bbcacbae83deb2f53
Alex Bradley <alexandre.bradley@shopify.com>
2022-01-10 13:31:11 -0500

    add other to extension points test

--- test/project_types/script/layers/application/extension_points_test.rb
+++ test/project_types/script/layers/application/extension_points_test.rb
@@ -86,7 +86,7 @@ describe Script::Layers::Application::ExtensionPoints do
       end
 
       it "should return all languages" do
-        assert_equal ["assemblyscript", "rust"], subject
+        assert_equal ["assemblyscript", "rust", "other"], subject
       end
     end
 
